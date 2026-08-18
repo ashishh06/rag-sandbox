5c490565351e603f0b7af78a68888974b98fa4d4
Ashish Sharma <ashishhariom14@gmail.com>
2026-08-18 22:40:10 +0530

    initial commit

---
+++ src/main/java/com/learn/ragSandbox/RagSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.learn.ragSandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagSandboxApplication.class, args);
+	}
+
+}
